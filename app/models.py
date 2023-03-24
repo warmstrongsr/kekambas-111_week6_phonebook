d28a99f3561ea45a9ea6b24cf26935ec8764aecf
Will Armstrong Sr <107271171+warmstrongsr@users.noreply.github.com>
2023-03-23 20:16:50 -0500

    changes to class model

--- app/models.py
+++ app/models.py
@@ -13,7 +13,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(75), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    posts = db.relationship('Address', backref='author')
+    address = db.relationship('Address', backref='user')
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -31,7 +31,7 @@ class User(db.Model, UserMixin):
 def get_a_user_by_id(user_id):
     return db.session.get(User, user_id)
 
-class Address(db.Model,UserMixin):
+class Address(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
@@ -39,7 +39,7 @@ class Address(db.Model,UserMixin):
     address = db.Column(db.String(75), nullable=False, unique=False)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id')) # SQL - FOREIGN KEY(user_id) REFERENCES user(id) SO HELPFUL!!
-
+    user = db.relationship(User, backref='addresses')
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         db.session.add(self)
