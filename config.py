69f155174a13806cc1eacee15ec508fa0b41b896
Will Armstrong Sr <107271171+warmstrongsr@users.noreply.github.com>
2023-07-06 20:56:59 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
+shh=shh
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'some-random-string'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
